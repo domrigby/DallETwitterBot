a0109b0d651af3a6d803563def0dc02c55f79b51
domrigby <domgdrigby@gmail.com>
2022-08-21 10:24:53 +0100

    reorganised files

--- pickleMaker.py
+++ pickleMaker.py
@@ -1,8 +1,9 @@
 import pickle
 
-title = 'Kanye_West_Lyrics.txt'
+title = 'signInDetails.pkl'
 
-with open(title,"rb") as f:
-    lines = f.readlines()
-    with open("Kanye_West_Lyrics.pkl","wb") as pickledF:
-        pickle.dump(lines,pickledF)
+logIn = {"api_key":"2w0mcXMnplO2z6XgbKK4hN6Ar", "api_secret_key":"EmW6vu3jHMCH1uzvfGbF9ndeAbJY3sZiDMGID1Mg2ri1rjRFMU",
+"access_token":"1560205890002747395-tHjCIyDmJksrmG4hi3uRJecmMfFsS9", "access_secret":"tsB3Ot9ZdcOhlrBfPg2FA7ORINz13FBvg76WYryB6h7U3"}
+
+with open(title,"wb") as pickledF:
+    pickle.dump(logIn,pickledF)
